f60ad186319452ddd4d93892416fec9a2f6ee637
proDEV <32644389+samy1406@users.noreply.github.com>
2022-07-03 18:17:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNum1 = Math.floor(Math.random()*6)+1;
-var randomimage1 = "images/dice"+randomNum1+".png";
+var randomimage1 = "dice"+randomNum1+".png";
 document.querySelector(".img1").setAttribute("src", randomimage1);
 var randomNum2 = Math.floor(Math.random()*6)+1;
-var randomimage2 = "images/dice"+randomNum2+".png";
+var randomimage2 = "dice"+randomNum2+".png";
 document.querySelector(".img2").setAttribute("src",randomimage2);
 
 if(randomNum1>randomNum2){
